07f116fa0e44862ac3548dd557091fe95e8efe09
Ba Thien LE <bathien.le.pro@gmail.com>
2022-01-10 14:01:38 +0100

    Add custom Cytomine Tile download class for PIMS

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# * Copyright (c) 2021. Ba Thien LE
+# * Copyright (c) 2022. Ba Thien LE
 # *
 # * Licensed under the Apache License, Version 2.0 (the "License");
 # * you may not use this file except in compliance with the License.
@@ -15,13 +15,18 @@
 # * limitations under the License.
 
 import numpy as np
+import os
+import PIL
 from shapely.affinity import affine_transform
 from shapely.geometry import Polygon
 
-from cytomine import CytomineJob
+from cytomine import Cytomine, CytomineJob
 from cytomine.models import Annotation, AnnotationCollection, ImageInstance
 
-from sldc import Logger, StandardOutputLogger, Segmenter, SSLWorkflowBuilder
+from sldc import (
+    Logger, StandardOutputLogger, Segmenter, SSLWorkflowBuilder, Tile,
+    TileExtractionException, TileTopology, alpha_rasterize
+)
 from sldc_cytomine import CytomineSlide, CytomineTileBuilder
 
 
@@ -33,6 +38,77 @@ class ExtendedCytomineSlide(CytomineSlide):
     @CytomineSlide.channels.getter
     def channels(self) -> int:
         return 1
+
+
+class CytominePIMSTile(Tile):
+    def __init__(
+        self, working_path, parent, offset, width, height,
+        tile_identifier=None, polygon_mask=None
+    ):
+        super().__init__(
+            parent, offset, width, height, tile_identifier=tile_identifier,
+            polygon_mask=polygon_mask
+        )
+
+        self._working_path = working_path
+
+    @property
+    def cache_filename(self):
+        image_instance = self.base_image.image_instance
+        x, y = self.abs_offset_x, self.abs_offset_y
+        width, height = self.width, self.height
+        zoom = self.base_image.zoom_level
+        return f'{image_instance.id}-{zoom}-{x}-{y}-{width}-{height}.png'
+
+    @property
+    def cache_filepath(self):
+        return os.path.join(self._working_path, self.cache_filename)
+
+    @property
+    def np_image(self):
+        try:
+            if not os.path.exists(self.cache_filepath) and not self.download_tile_image():
+                raise TileExtractionException(
+                    f"Cannot fetch tile at for '{self.cache_filename}'."
+                )
+
+            np_array = np.asarray(PIL.Image.open(self.cache_filepath)).squeeze()
+
+            if np_array.shape[:2] != (self.height, self.width) \
+                    or (self.channels > 1 and (np_array.ndim < 3 or np_array.shape[2] != self.channels)) \
+                    or (self.channels == 1 and np_array.ndim > 2 and np_array.shape[2] != self.channels):
+                raise TileExtractionException(
+                    f"Fetched image has invalid size : {np_array.shape} instead "
+                    f"of {(self.width, self.height, self.channels)}"
+                )
+
+            if np_array.ndim == 3 and np_array.shape[2] == 4:
+                np_array = np_array[:, :, :3]
+
+            np_array = np_array.astype("uint8")
+            return self.add_polygon_mask(np_array)
+        except IOError as e:
+            raise TileExtractionException(str(e))
+
+    def add_polygon_mask(self, image):
+        try:
+            return alpha_rasterize(image, self.polygon_mask)
+        except:
+            return image
+
+    def download_tile_image(self):
+        slide = self.base_image
+        filepath = slide.image_instance.path
+        topology = TileTopology(slide, None, max_width=256, max_height=256)
+        col_tile = self.abs_offset_x // 256
+        row_tile = self.abs_offset_y // 256
+        tile_index = col_tile + row_tile * topology.tile_horizontal_count
+        _slice = slide.slice_instance
+
+        url = f'{_slice.imageServerUrl}/image/{filepath}/tile/' \
+              f'zoom/{slide.api_zoom_level}/ti/{tile_index}.png'
+
+        return Cytomine.get_instance().download_file(url, self.cache_filepath)
 
 
 class ThresholdSegmenter(Segmenter):
@@ -63,7 +139,9 @@ def main(argv):
         builder = SSLWorkflowBuilder()
         builder.set_background_class(0)
         builder.set_tile_size(512, 512)
-        builder.set_tile_builder(CytomineTileBuilder(working_path))
+        builder.set_tile_builder(
+            CytomineTileBuilder(working_path, tile_class=CytominePIMSTile)
+        )
         builder.set_logger(StandardOutputLogger(level=Logger.WARNING))
         builder.set_segmenter(ThresholdSegmenter(cj.parameters.threshold))
 
